38fa9e0f55e8415f24a3e1c2e79b9fbc6326047d
costaaja <costaaja017@gmail.com>
2026-09-15 20:32:52 +0700

    fix config

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,6 @@
 <?php
 // Konfigurasi koneksi ke database
 $host = "localhost"; // Host MySQL
-// $username = "root"; // Username MySQL
-// $password = ""; // Password MySQL
-// $database = "newsportal"; // Nama Database
 $username = "u828472685_cakrawala"; // Username MySQL
 $password = "Cakrawala123!"; // Password MySQL
 $database = "u828472685_cakrawala"; // Nama Database
@@ -13,17 +10,7 @@ date_default_timezone_set('Asia/Jakarta');
 ini_set('date.timezone', 'Asia/Jakarta');
 
 // Buat koneksi ke database
-// Buat koneksi ke database
-echo "<pre>";
-echo "CONFIG YANG DIPAKAI\n";
-echo "HOST: " . $host . "\n";
-echo "USER: " . $username . "\n";
-echo "DB: " . $database . "\n";
-echo "</pre>";
-exit;
-
 $con = mysqli_connect($host, $username, $password, $database);
-// $con = mysqli_connect($host, $username, $password, $database);
 
 // Periksa koneksi
 if (!$con) {
